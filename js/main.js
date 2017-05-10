bad95dc217303d55518e7a74770d436a25874e84
Gamaliel Padillo <gamalielgad1234@gmail.com>
2017-05-10 00:47:18 -0700

    Add headshot, remove js, add newer sections

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-  $('.slider').slider({
-    height: $(window).height(),
-    indicators: false,
-    interval: 5000
-  });
-
-  $('.project-slider').slick({
-    arrows: true,
-    slidesToShow: 2
-  });
+  
 });
