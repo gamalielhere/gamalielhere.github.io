ba04a89e8689e4528883dad4e38f1b5c2a4de6de
Gamaliel Padillo <gamalielgad1234@gmail.com>
2018-04-30 21:07:24 -0700

    Set up header with react-router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import CssBaseline from "material-ui/CssBaseline";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import React, { PureComponent } from "react";
-import theme from "../../config/theme";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import theme from "../../config/theme.js";
 import "../../css/App.css";
 import HeaderContainerComponent from "../HeaderComponent";
 
@@ -10,9 +11,17 @@ class App extends PureComponent {
     return (
       <React.Fragment>
         <CssBaseline />
-        <MuiThemeProvider theme={theme}>
-          <HeaderContainerComponent />
-        </MuiThemeProvider>
+        <Router>
+          <MuiThemeProvider theme={theme}>
+            <HeaderContainerComponent />
+            <Switch>
+              <Route exact path="/" component={() => <div> Home! </div>} />
+              <Route exact path="/projects" component={() => <div> Projects! </div>} />
+              <Route exact path="/about" component={() => <div> About! </div>} />
+              <Route exact path="/connect" component={() => <div> Connect! </div>} />
+            </Switch>
+          </MuiThemeProvider>
+        </Router>
       </React.Fragment>
     );
   }
