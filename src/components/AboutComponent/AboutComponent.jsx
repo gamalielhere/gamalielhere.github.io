e7225fc00fa5db0a5024e3ad06cd82f573eebb0a
Gamaliel Padillo <gamalielgad1234@gmail.com>
2018-05-02 22:03:28 -0700

    Add files

---
+++ src/components/AboutComponent/AboutComponent.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class AboutComponent extends PureComponent {
+  render() {
+    return <div className="pageContainer">About!</div>;
+  }
+}
+
+export default AboutComponent;
---
+++ src/components/ConnectComponent/ConnectComponent.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class ConnectComponent extends PureComponent {
+  render() {
+    return <div className="pageContainer">Connect!</div>;
+  }
+}
+
+export default ConnectComponent;
