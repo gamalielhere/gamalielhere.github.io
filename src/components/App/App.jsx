134bfedf54cbb895b53c84f139a9fa00881e12b0
Gamaliel Padillo <gamalielgad1234@gmail.com>
2018-05-02 22:03:18 -0700

    Remove inline and unused stuff, update names, add favicon

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,42 @@
 import CssBaseline from "material-ui/CssBaseline";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import React, { PureComponent } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import theme from "../../config/theme.js";
-import "../../css/App.css";
-import HeaderContainerComponent from "../HeaderComponent";
-import FooterContainerComponent from "../FooterComponent";
+import AboutComponent from "../AboutComponent";
+import ConnectComponent from "../ConnectComponent";
+import FooterComponent from "../FooterComponent";
+import HeaderComponent from "../HeaderComponent";
+import HomeComponent from "../HomeComponent";
+import ProjectComponent from "../ProjectComponent";
 
 class App extends PureComponent {
   render() {
-    const styles = {
-      height: 'calc(100vh - 102px)'
-    }
     return (
       <React.Fragment>
         <CssBaseline />
         <Router>
           <MuiThemeProvider theme={theme}>
-            <HeaderContainerComponent />
+            <HeaderComponent />
             <Switch>
-              <Route exact path="/" component={() => <div style={styles}> Home! </div>} />
-              <Route exact path="/projects" component={() => <div style={styles}> Projects! </div>} />
-              <Route exact path="/about" component={() => <div style={styles}> About! </div>} />
-              <Route exact path="/connect" component={() => <div style={styles}> Connect! </div>} />
+              <Route exact path="/" component={() => <HomeComponent />} />
+              <Route
+                exact
+                path="/projects"
+                component={() => <ProjectComponent />}
+              />
+              <Route
+                exact
+                path="/about"
+                component={() => <AboutComponent />}
+              />
+              <Route
+                exact
+                path="/connect"
+                component={() =>  <ConnectComponent />}
+              />
             </Switch>
-            <FooterContainerComponent />
+            <FooterComponent />
           </MuiThemeProvider>
         </Router>
       </React.Fragment>
