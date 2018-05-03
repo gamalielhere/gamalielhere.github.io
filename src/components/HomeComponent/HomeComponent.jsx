5313ac41edd9cd832ff1a442932f18e43b4e0eea
Gamaliel Padillo <gamalielgad1234@gmail.com>
2018-05-02 22:03:28 -0700

    Add files

---
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class HomeComponent extends PureComponent {
+  render() {
+    return <div className="pageContainer">Home!</div>;
+  }
+}
+
+export default HomeComponent;
---
+++ src/components/ProjectComponent/ProjectComponent.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class ProjectComponent extends PureComponent {
+  render() {
+    return <div className="pageContainer">Projects!</div>;
+  }
+}
+
+export default ProjectComponent;
