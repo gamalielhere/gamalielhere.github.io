adf10a7594ebb91ac9851855b45b7112698e4294
Gamaliel Padillo <gamalielgad1234@gmail.com>
2018-05-05 13:12:01 -0700

    Add scss lint, add page router animation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import CssBaseline from "material-ui/CssBaseline";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import React, { PureComponent } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import theme from "../../config/theme.js";
 import AboutComponent from "../AboutComponent";
 import ConnectComponent from "../ConnectComponent";
@@ -9,39 +10,31 @@ import FooterComponent from "../FooterComponent";
 import HeaderComponent from "../HeaderComponent";
 import HomeComponent from "../HomeComponent";
 import ProjectComponent from "../ProjectComponent";
+import '../../css/App/index.css';
 
 class App extends PureComponent {
   render() {
+    const {location} = this.props;
     return (
       <React.Fragment>
         <CssBaseline />
-        <Router>
-          <MuiThemeProvider theme={theme}>
-            <HeaderComponent />
-            <Switch>
-              <Route exact path="/" component={() => <HomeComponent />} />
-              <Route
-                exact
-                path="/projects"
-                component={() => <ProjectComponent />}
-              />
-              <Route
-                exact
-                path="/about"
-                component={() => <AboutComponent />}
-              />
-              <Route
-                exact
-                path="/connect"
-                component={() =>  <ConnectComponent />}
-              />
-            </Switch>
-            <FooterComponent />
-          </MuiThemeProvider>
-        </Router>
+        <MuiThemeProvider theme={theme}>
+          <HeaderComponent />
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={300} classNames="pageSlider" mountOnEnter={true} unmountOnExit={true}>
+              <Switch location={location}>
+                <Route exact path="/" component={HomeComponent} />
+                <Route exact path="/projects" component={ProjectComponent} />
+                <Route exact path="/about" component={AboutComponent} />
+                <Route exact path="/connect" component={ConnectComponent} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+          <FooterComponent />
+        </MuiThemeProvider>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
