bccfaaec67eaac4c52def947df3972e58f83c6bd
Gamaliel Padillo <gamalielgad1234@gmail.com>
2018-05-21 20:33:19 -0700

    Trigger build

--- src/components/AboutComponent/AboutComponent.jsx
+++ src/components/AboutComponent/AboutComponent.jsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from "react";
 
 class AboutComponent extends PureComponent {
   render() {
-    return <div className="pageContainer">About Page!</div>;
+    return <div className="pageContainer">About!</div>;
   }
 }
 
