75564c520da5277eb91c6ded84f313420e99a6e3
Gamaliel Padillo <gamalielgad1234@gmail.com>
2018-06-02 14:17:41 -0700

    Recompile

--- src/components/ConnectComponent/ConnectComponent.jsx
+++ src/components/ConnectComponent/ConnectComponent.jsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from "react";
 
 class ConnectComponent extends PureComponent {
   render() {
-    return <div className="pageContainer">Connect Component!</div>;
+    return <div className="pageContainer">Connect!</div>;
   }
 }
 
